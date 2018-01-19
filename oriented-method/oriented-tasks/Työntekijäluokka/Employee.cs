042e9f311509a5a847cb1ecc12d8f296b33b25f2
jenna <jenna.pesu@gmail.com>
2018-01-19 11:08:36 +0200

    Työntekijäluokka kesken

---
+++ oriented-method/oriented-tasks/Työntekijäluokka/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Työntekijäluokka
+{
+    class Employee
+    {
+        public string Name;
+        public int Id;
+        public string Position;
+        public int Salary; 
+
+        public Employee()
+        {
+            Name = "unknown";
+            Id = 0;
+            Position = "unknown";
+            Salary = 0; 
+        } 
+
+        public Employee(string name, int id, string position, int salary)
+        {
+            Name = name;
+            Id = id;
+            Position = position;
+            Salary = salary; 
+        } 
+        public void CompareSalary(Employee employee)
+        {
+            if (Salary > employee.Salary)
+                Console.WriteLine($"{Name} palkka on suurempi, kuin {employee.Name}");
+            else
+                Console.WriteLine($"{employee.Name} palkka on suurempi, kuin {Name} ");
+
+        } 
+        public void PrintEmployeeInfo()
+        {
+            Console.WriteLine($"Työntekijä\nName {Name}\nPalkka {Salary} "); 
+
+        }
+    }
+}
---
+++ oriented-method/oriented-tasks/Työntekijäluokka/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Työntekijäluokka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee työntekijä1 = new Employee("Anton", 1,"Toimitusjohtaja ", 6000);
+            Employee työntekijä2 = new Employee("Dan",2, "Myyjä", 5000);
+            Employee työntekijä3 = new Employee("Kyle", 3,"Omistaja",7000 );
+
+            työntekijä1.CompareSalary(työntekijä2);
+            työntekijä2.CompareSalary(työntekijä3);
+
+            työntekijä1.PrintEmployeeInfo();
+            työntekijä2.PrintEmployeeInfo();
+            työntekijä3.PrintEmployeeInfo();
+
+            Employee[] employees = new Employee[3];
+            employees[0] = työntekijä1;
+            employees[1] = new Employee("Heikki", 456, "Kirjailija", 3456);
+            employees[2] = työntekijä3;
+
+            Console.ReadKey(); 
+          
+
+
+        }
+    }
+}
