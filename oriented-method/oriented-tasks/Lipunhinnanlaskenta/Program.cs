cacd3fe54048832dbe5ca97d9542fbf56fbf45d8
jenna <jenna.pesu@gmail.com>
2018-01-25 11:07:01 +0200

    Lipunhinnanlaskenta kesken

---
+++ oriented-method/oriented-tasks/Lipunhinnanlaskenta/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lipunhinnanlaskenta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ oriented-method/oriented-tasks/Lipunhinnanlaskenta/Ticket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lipunhinnanlaskenta
+{
+    class Ticket
+    {
+        public string Customer;
+        public int Price; 
+        public int Age;
+        public string Mtk-jäsen; 
+        public string Opiskelija;
+        public string Varusmies;
+
+        public void Ticket 
+
+
+
+    }
+}
