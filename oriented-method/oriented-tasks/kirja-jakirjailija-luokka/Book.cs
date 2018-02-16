3d78ca750742133a60151c7df3ac152ea80280c7
jenna <jenna.pesu@gmail.com>
2018-02-16 10:37:24 +0200

    kirja-jakirjailijaluokka kesken

---
+++ oriented-method/oriented-tasks/kirja-jakirjailija-luokka/Book.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kirja_jakirjailija_luokka
+{
+    class Book
+{
+    public string Title;
+    public string Author;
+    public string Publicer;
+        private int price;
+        public static string Theme;
+
+        public int Price { get => price; set => price = value; } 
+         {
+       
+        public Book()
+    {
+        Title = "unknown";
+        Author = "unknown";
+        Publicer = "unknown";
+        Price = 0;
+
+    }
+
+    public Book(string title, string author, string publicer, int price, string theme)
+    {
+        Title = title;
+        Author = author;
+        Publicer = publicer;
+        Price = price;
+        Theme = theme;
+    }
+    public void GetBook(string bookTitle)
+    {
+        if (Title == bookTitle)
+            Console.WriteLine($"Kirja löytyi {Title}, Kirjailija {Author}, Kustantaja {Publicer}, hinta {Price}"); 
+    }
+
+        public static void ChangeTheme(string newTheme)
+        {
+            Theme = newTheme;
+
+        }
+
+        public static 
+        { 
+            if ( )
+ }   
+}
---
+++ oriented-method/oriented-tasks/kirja-jakirjailija-luokka/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace kirja_jakirjailija_luokka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book newBook = new Book("kirja", "Anton", "Mattel Studios", 20, "komedia");
+            newBook.GetBook("kirja");
+
+            Book.ChangeTheme("kauhu");
+
+           
+        }
+    }
+}
